8ce1a87534f5bb28deb34b30aaa5071e579732f1
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-02-03 20:56:04 +0530

    add the public construtor file

---
+++ PublicCons.java
@@ -0,0 +1,16 @@
+
+
+class Car
+{
+    public Car()
+    {
+        System.out.println("This is defualt construto");
+    }
+}
+public class PublicCons
+{
+    public static void main(String[] args) {
+      
+        Car car=new Car(); // at the time of object creation default construtor is called.
+    }
+}
